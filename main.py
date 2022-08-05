1988fb39130b46f4df48d77d6c1749503b40f7e1
Kelvin Hong <kh.boon2@gmail.com>
2022-08-05 10:47:46 +0800

    Added cosine similarity and visualization.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.metrics.pairwise import cosine_similarity
-
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
 
 def preprocess(file_name, index = "movieId", save_as = None) -> pd.DataFrame:
     df = pd.read_csv(file_name)
@@ -12,37 +13,57 @@ def preprocess(file_name, index = "movieId", save_as = None) -> pd.DataFrame:
     # Aggregate data into 2-dimensional matrix
     assert index in ["userId", "movieId"]
     if index == "movieId":
-        user_movie_mat = df.pivot(
+        mat = df.pivot(
             index="movieId",
             columns="userId",
             values="rating"
         )
     else:
-        user_movie_mat = df.pivot(
+        mat = df.pivot(
             index="userId",
             columns="movieId",
             values="rating"
         )
-    print("User-Movie relation table has shape ", user_movie_mat.shape)
-    total_count = user_movie_mat.shape[0] * user_movie_mat.shape[1]
-    print("total cells", total_count)
-    missing_count = user_movie_mat.isna().sum().sum()
-    print(f"Missing values count is {missing_count}, makes up {round(100 * missing_count / total_count, 2)}% of the data.")
-    
     if save_as is not None:
-        user_movie_mat.to_csv(save_as)
+        mat.to_csv(save_as)
 
-    return user_movie_mat
+    return mat
+
+def visualize_cosine(df, csv, png, fmt = "%.3f"):
+    sim_mat = cosine_similarity(df.fillna(0))
+    np.savetxt(csv, sim_mat, delimiter=",", fmt=fmt)
+    print(f"Dataframe shape {df.shape}, siilarity matrix {sim_mat.shape}.")
+    zero_count = np.count_nonzero(sim_mat == 0)
+    print(f"Count of zeros in similarity matrix is {zero_count}, {round(100 * zero_count / (sim_mat.shape[0] * sim_mat.shape[1]), 2)}%.")
+    plt.matshow(sim_mat)
+    plt.savefig(png)
+    print(f"Similarity matrix saved at {os.path.abspath(csv)} .")
+    print(f"Similarity visualization saved at {os.path.abspath(png)} .")
+    
 
 if __name__ == "__main__":
     os.makedirs("./output/", exist_ok=True)
     
     file_name = "./ml-latest-small/ratings.csv"
     # Pivot rating.csv into a 2D-matrix.
-    processed_df = preprocess(file_name, save_as = "./output/user-movie-rating.csv")
-    sim_mat = cosine_similarity(processed_df.fillna(0))
-    print("Dataframe shape is", processed_df.shape)
-    print("Similarity matrix shape is", sim_mat.shape)
-    print(sim_mat[:5, :5])
-    print(type(sim_mat))
-    print("Count of zeros in similarity matrix is ", np.count_nonzero(sim_mat == 0))
+    item_row_df = preprocess(file_name, index="movieId", save_as = "./output/movie-user-rating.csv")
+    user_row_df = preprocess(file_name, index="userId", save_as = "./output/user-movie-rating.csv")
+    
+    # Cosine similarity based on user and item separately.
+    print("Item based cosine similarity:")
+    visualize_cosine(
+        item_row_df, 
+        csv = "./output/itembased_similarity.csv", 
+        png = "./output/itembased_corr.png", 
+    )
+
+    print("User based cosine similarity:")
+    visualize_cosine(
+        user_row_df, 
+        csv = "./output/userbased_similarity.csv", 
+        png = "./output/userbased_corr.png", 
+    )
+
+    # Pearson Similarity based on user and item separately.
+
+    # K nearest neighbor method base on user and item separately.
--- utils.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity
-
-def similarity(df, method = "cosine"):
-    if method == "cosine":
-        ret = cosine_similarity(df)
-    
-    return ret
