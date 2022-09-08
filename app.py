0f0f289a28457fd37c96071427dfdeb366f67c5a
Kelvin Hong <kh.boon2@gmail.com>
2022-09-08 17:49:58 +0800

    Add script for recommendation system

---
+++ app.py
@@ -0,0 +1,5 @@
+"""
+This script is intended to run Recommendation System using various techniques. 
+We would do Cosine, Pearson, KNN, KNN with mean, KNN with Z-score.
+These terms might not be used in the same stage, but will be used nonetheless. 
+"""
