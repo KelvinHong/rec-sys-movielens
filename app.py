3f7e51726c56d981e39c3d789df7bc70abc6ec2d
Kelvin Hong <kh.boon2@gmail.com>
2022-09-20 14:50:50 +0800

    Minor improvement

--- app.py
+++
@@ -1,5 +0,0 @@
-"""
-This script is intended to run Recommendation System using various techniques. 
-We would do Cosine, Pearson, KNN, KNN with mean, KNN with Z-score.
-These terms might not be used in the same stage, but will be used nonetheless. 
-"""
